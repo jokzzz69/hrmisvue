40e23ddc8c5818a8f3ed78f1c6aaeb5fab8d52a2
Joel <sangwayjoel@gmail.com>
2023-09-21 15:07:59 +0800

    weeeshooo

--- src/composables/composables-biometricsdevice.js
+++ src/composables/composables-biometricsdevice.js
@@ -15,88 +15,101 @@ export default function useBioDevice(){
 
 	const getPage = async() =>{
 		axios.defaults.withCredentials = true;	
-		let response = await axios.get('/v1/api/biometricsdevice');
-		batch.value = response.data.data;
+		await axios.get('http://localhost:8001/bio/api/biometricsdevice').then((response) =>{
+			batch.value = response.data.data;
+		});		
 	}
+
+
 	const getBioTimeMain = async () => {
-		axios.defaults.withCredentials = true;	
-        let response = await axios.get('/v1/api/getmainbiometrics');
-		mainbio.value = response.data.data;
+		axios.defaults.withCredentials = true;
+		await axios.get('http://localhost:8001/bio/api/getmainbiometrics').then((response) =>{
+			mainbio.value = response.data.data;
+		});			        
     }  	   	
     const getBioTimeRFMIC = async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/getrfimcbiometrics/`);
-        rfimcbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/getrfimcbiometrics').then((response) =>{
+			rfimcbio.value = response.data.data; 
+		});	
+
     }
     const getBioTimeFMRED= async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/getfmredbiometrics/`);
-        fmredbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/getfmredbiometrics').then((response) =>{
+			fmredbio.value = response.data.data; 
+		});	
+
     }
     const getBioTimeBENGUET = async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/getbenguetbiometrics/`);
-        benguetbio.value = response.data.data;   
+        await axios.get('http://localhost:8001/bio/api/getbenguetbiometrics').then((response) =>{
+			benguetbio.value = response.data.data; 
+		});	
     }
 
 
     const currRFMIC = async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/currfimcbiometrics/`);
-        rfimcbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/currfimcbiometrics').then((response) =>{
+			rfimcbio.value = response.data.data; 
+		});	
+
     }
     const currFMRED= async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/curfmredbiometrics/`);
-        fmredbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/curfmredbiometrics').then((response) =>{
+			fmredbio.value = response.data.data; 
+		});	
+ 
     }
     const currBENGUET = async () => {
     	axios.defaults.withCredentials = true;	
-    	let response = await axios.get(`/v1/api/curbenguetbiometrics/`);
-        benguetbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/curbenguetbiometrics').then((response) =>{
+			benguetbio.value = response.data.data; 
+		});	
     }
-
-
 
 
     const setBioTimeMain = async () => {
     	axios.defaults.withCredentials = true;	
-        let response = await axios.get(`/v1/api/biometricdevice/`);
-        mainbio.value = response.data.data;   
+    	await axios.get('http://localhost:8001/bio/api/biometricdevice').then((response) =>{
+			mainbio.value = response.data.data; 
+		});	 
     }
     const setBioTimeRFMIC = async () => {
     	axios.defaults.withCredentials = true;	
-        let response = await axios.post(`/v1/api/setrfimcbiometrics/`);
-        rfimcbio.value = response.data.data;   
+
+    	await axios.post('http://localhost:8001/bio/api/setrfimcbiometrics').then((response) =>{
+			rfimcbio.value = response.data.data; 
+		});	 
+
+
     }
     const setBioTimeFMRED = async () => {
     	axios.defaults.withCredentials = true;	
-        let response = await axios.post(`/v1/api/setfmredbiometrics/`);
-        rfimcbio.value = response.data.data;   
+
+    	await axios.post('http://localhost:8001/bio/api/setfmredbiometrics').then((response) =>{
+			fmredbio.value = response.data.data; 
+		});	 
+
     }
     const setBioTimeBENGUET = async () => {
     	axios.defaults.withCredentials = true;	
-        let response = await axios.post(`/v1/api/setbenguetbiometrics/`);
-        rfimcbio.value = response.data.data;   
+
+    	await axios.post('http://localhost:8001/bio/api/setbenguetbiometrics').then((response) =>{
+			benguetbio.value = response.data.data; 
+		});	 
+
+
     }
 	
 
 	const getmainbiometricsuser = async() => {
 		axios.defaults.withCredentials = true;	
-		let response = await axios.get(`/v1/api/getmainbiouser/`);
-        mainbiousers.value = response.data.data;   
-
-        //let response = await axios.get('https://hrmis.bfarcar.da.gov.ph/v1/api/salarygradegroups');
-        //console.log(response);
-		//http://172.168.8.15:8080/api_v2/employees
-		//https://hrmis.bfarcar.da.gov.ph/v1/api/salarygradegroups
-        // await axios.get('http://172.168.8.15:8080/api_v2/employees',{
-        // 	headers: {
-        // 		Authorization: `Token 199e60e247a9c5792dfbb546b27541b4b7839d9004e115d6cd55bcddf734456a`
-        // 	}
-        // })
-        // .then(res => console.log(res))
-        // .catch(err => console.log(err));
+		await axios.get('http://localhost:8001/bio/api/getmainbiouser').then((response) =>{
+			mainbiousers.value = response.data.data; 
+		});	
 
 	
 	}
